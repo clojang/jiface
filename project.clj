1efa6da1d321650f03376fdf08ddc7d02c0ffdc8
Duncan McGreggor <oubiwann@gmail.com>
2016-04-30 17:33:15 -0500

    Bumped version number.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clojang/jiface "0.1.0"
+(defproject clojang/jiface "0.1.1"
   :description "A Clojure-idiomatic wrapper around Erlang's JInterface"
   :url "https://github.com/clojang/jiface"
   :scm {

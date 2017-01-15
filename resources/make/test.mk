c0985159d783d7bb057f6c079d740d3859e36c9d
Duncan McGreggor <oubiwann@gmail.com>
2017-01-15 02:00:11 -0600

    Updated make targets (fixed testing profile).

--- resources/make/test.mk
+++ resources/make/test.mk
@@ -1,11 +1,11 @@
 check:
-	@lein with-profile +test test :unit
+	@lein with-profile +testing test :unit
 
 check-system:
-	@lein with-profile +test test :system
+	@lein with-profile +testing test :system
 
 check-integration:
-	@lein with-profile +test test :integration
+	@lein with-profile +testing test :integration
 
 check-all:
-	@lein with-profile +test test :all
+	@lein with-profile +testing test :all

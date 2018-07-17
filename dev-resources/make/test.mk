691066b8c08a28ca3411fe7bde6067e890a99e66
Duncan McGreggor <oubiwann@gmail.com>
2018-07-17 18:01:45 -0500

    Update to make target for change in profile.

--- dev-resources/make/test.mk
+++ dev-resources/make/test.mk
@@ -8,7 +8,7 @@ check-integration:
 	@lein with-profile +test test :integration
 
 check-deps:
-	@lein with-profile +test do ancient check :all
+	@lein with-profile +lint do ancient check :all
 
 check-all: check-deps
 	@lein with-profile +test test :all
